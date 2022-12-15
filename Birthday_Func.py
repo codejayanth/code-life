5f702f5350d62bc2dd498969988aa9af00f9caa6
jayanth <42774204+codejayanth@users.noreply.github.com>
2022-12-15 13:37:15 +0100

    Update Birthday_Func.py

--- Birthday_Func.py
+++ Birthday_Func.py
@@ -181,32 +181,7 @@ class Birthday:
         
                 
     
-    
-from fastapi import FastAPI
-#app = from fastapi import FastAPI
-app = FastAPI()
-
-@app.get("/my-first-api")
-def hello(name: str):
-  return {'Hello ' + name + '!'}         
-            
-            
-    
-
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
-@app.route('/post', methods=['POST'])
-def post():
-    return "recived: {}".format(request.form)
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8050)            
+        
                 
                 
         
